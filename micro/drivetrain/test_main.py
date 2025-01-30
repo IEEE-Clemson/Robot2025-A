897dc5c3f95f6c4ae9e62a53594a061795c48e3a
Connor Belli <connorbelli2003@gmail.com>
2025-01-30 18:11:20 -0500

    Fixed wrong encoder ppr being used

--- micro/drivetrain/test_main.py
+++ micro/drivetrain/test_main.py
@@ -5,7 +5,6 @@ import utime
 from machine import Timer
 
 motor = PIMotor(0, 3, 2, 15, 14)
-motor.cprad = 2800
 voltage = 12
 throttle = 1
 motor.ff = 0.3
@@ -13,7 +12,7 @@ motor.p = 2.0
 motor.i = 2.0
 motor.max_i_acc = 1 / motor.i
 motor.drive_raw(0)
-utime.sleep(5)
+utime.sleep(2)
 i = 0
 
 freq = 200
@@ -22,12 +21,13 @@ control_tim = Timer()
 def control_update(timer):
     motor.update(dt)
 
-control_tim.init(freq=200, mode=Timer.PERIODIC, callback=control_update) #type: ignore
+control_tim.init(freq=freq, mode=Timer.PERIODIC, callback=control_update) #type: ignore
 
 while True:
     utime.sleep_ms(500)
     print(motor.cur_vel)
     print(motor.out)
+    print()
     # Estimate for ff in PI controller
     if motor.cur_vel > 0.1:
         print("FF value", throttle / motor.cur_vel)
