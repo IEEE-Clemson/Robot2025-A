d2229bd6e2c3ffacb5fc27f1e3ea2292849bb7e0
Jacob Daniel <jrdnl@g.clemson.edu>
2025-02-17 22:53:23 -0500

    Expanding controls class

--- BMI270/controlsClass.py
+++ BMI270/controlsClass.py
@@ -1,116 +1,228 @@
-
-
 from machine import Pin, PWM, I2C # type: ignore 
 from registerDefinitions import * # type: ignore 
 
 
 
 
-class PowerControl:
-    def __init__(self, serialDevice: I2C) -> None:
+class PWR_Configure:
+    def __init__(self, serial_device: I2C) -> None:
         
         
-        self.auxStatus = 0
-        self.gyroStatus = 0
-        self.accelStatus = 0
-        self.tempStatus = 0
+        self.adv_power_save = 0x00
+        self.fifo_self_wake_up =0x00
+        self.fup_en = 0x00
+    
         
-        self.serialDevice = serialDevice
+        
+        self.serial_device = serial_device
+        
+        return None
+    
+    def read_regsiter(self) -> int:
+        
+        current_val = self.serial_device.readfrom_mem(I2C_PRIM_ADDR, PWR_CONF, 1)
+        current_val = int.from_bytes(current_val)
+        
+        
+        return current_val
+    
+    
+    def adv_power_toggle(self, value: int) -> None:
+        
+        current_register = self.read_regsiter()
+        
+        if(value == 0x00):
+            current_register &= ~BIT_0
+        elif(value == 0x01):
+            current_register |= BIT_0 
+        else:
+            return None 
+        
+        
+        self.adv_power_save = value 
+        self.serial_device.writeto_mem(I2C_PRIM_ADDR, PWR_CONF, current_register)
         
         
         return None 
-    
 
-    def readRegister(self, registerAddress: int) -> int:
-        """
-        Simple utility function that returns the integer value of a read register value on the IMU.
-        """
+    def fifo_toggle(self, value: int) -> None:
         
-        currentRegister = self.serialDevice.readfrom_mem(I2C_PRIM_ADDR, registerAddress, 1)
-        currentRegister = int.from_bytes(currentRegister)
-        
-        return currentRegister
-    
-    
-    
-    def auxToggle(self) -> None:
-        """
-        Toggles whether the auxillary sensor is enabled ON/OFF
-        """
-        currentRegister = self.readRegister(registerAddress=PWR_CTRL, serialDevice=self.serialDevice)
-                
-        if(self.auxStatus == 1):
-            self.auxStatus = 0
-            currentRegister &= ~BIT_0
+        current_register = self.read_regsiter()
+        if(value == 0x00):
+            current_register &= ~BIT_1 
+        elif(value == 0x01):
+            current_register |= BIT_1 
         else:
-            self.auxStatus = 1 
-            currentRegister |= BIT_0 
-            
+            return None 
         
-        self.serialDevice.writeto_mem(I2C_PRIM_ADDR, PWR_CTRL, currentRegister)
+        
+        self.fifo_self_wake_up = value 
+        self.serial_device.writeto_mem(I2C_PRIM_ADDR, PWR_CONF, current_register)
         
         
         return None 
     
     
-    def gyroToggle(self) -> None:
-        """
-        Toggles whether the gyroscope is enabled ON/OFF
-        """
-        currentRegister = self.readRegister(registerAddress=PWR_CTRL, serialDevice=self.serialDevice)
+    
+    def fup_toggle(self, value: int) -> None:
         
-        if(self.gyroStatus == 1):
-            self.gyroStatus = 0
-            currentRegister &= ~BIT_1
+        current_register = self.read_regsiter()
+        if(value == 0x00):
+            current_register &= ~BIT_2 
+        elif(value == 0x01):
+            current_register |= BIT_2 
         else:
-            self.gyroStatus = 1 
-            currentRegister |= BIT_1 
-            
-        self.serialDevice.writeto_mem(I2C_PRIM_ADDR, PWR_CTRL, currentRegister)
+            return None 
+        
+        
+        self.fup_en = value 
+        self.serial_device.writeto_mem(I2C_PRIM_ADDR, PWR_CONF, current_register)
+        
+        
         
         
         return None 
-    
-    
-    def accelToggle(self) -> None:
-        """
-        Toggles whether the acceleration is enabled ON/OFF
-        """
-        
-        currentRegister = self.readRegister(registerAddress=PWR_CTRL, serialDevice=self.serialDevice)
-        
-        if(self.accelStatus == 1):
-            self.accelStatus = 0
-            currentRegister &= ~BIT_2 
-        else:
-            self.accelStatus = 1 
-            currentRegister |= BIT_2
-        
-                
-        self.serialDevice.writeto_mem(I2C_PRIM_ADDR, PWR_CTRL, currentRegister)
-        
-        
-        return None 
-    
-    
-    def tempToggle(self) -> None:
-        """
-        Toggles whether the temperature sensor is enabled ON/OFF
-        """
-        
-        currentRegister = self.readRegister(registerAddress=PWR_CTRL, serialDevice=self.serialDevice)
-        
-        if(self.tempToggle == 1):
-            self.tempStatus = 0 
-            currentRegister &= ~BIT_3 
-        else:
-            self.tempStatus = 1 
-            currentRegister |= BIT_3 
-        
 
-        self.serialDevice.writeto_mem(I2C_PRIM_ADDR, PWR_CTRL, currentRegister)
+
+class PWR_Control:
+    def __init__(self, serial_device: I2C) -> None:
+        
+        
+        self.aux_en = 0x00
+        self.gyro_en = 0x00
+        self.acc_en = 0x00
+        self.temp_en = 0x00
+        
+        
+        self.serial_device = serial_device 
+        
+        
+        
+        return None 
+    
+    def read_regsiter(self, address: int) -> int:
+        
+        current_val = self.serial_device.readfrom_mem(I2C_PRIM_ADDR, address, 1)
+        current_val = int.from_bytes(current_val)
+        
+        
+        return current_val
+    
+    
+    def auxillary_enable(self, value: int) -> None:
+        
+        current_register = self.read_regsiter(PWR_CTRL)
+        
+        if(value == 0x00):
+            current_register &= ~BIT_0
+            
+        elif(value == 0x01):
+            current_register |= BIT_0
+            
+        else:
+            return None 
+        
+        
+        
+        self.aux_en = value 
+        self.serial_device.writeto_mem(I2C_PRIM_ADDR, PWR_CTRL, current_register)
         
         return None 
     
     
+    def gyroscope_enable(self, value: int) -> None:
+        
+        current_register = self.read_regsiter(PWR_CTRL)
+        
+        if(value == 0x00):
+            current_register &= ~BIT_1
+        elif(value == 0x01):
+            current_register |= BIT_1 
+        else:
+            return None 
+        
+        self.gyro_en = value 
+        self.serial_device.writeto_mem(I2C_PRIM_ADDR, PWR_CTRL, current_register)
+        
+        return None 
     
+    
+    def accel_enable(self, value: int) -> None:
+        
+        current_register = self.read_regsiter(PWR_CTRL)
+        
+        if(value == 0x00):
+            current_register &= ~BIT_2 
+        elif(value == 0x01):
+            current_register |= BIT_2 
+        else:
+            return None 
+        
+        
+        
+        self.acc_en = value 
+        self.serial_device.writeto_mem(I2C_PRIM_ADDR, PWR_CTRL, current_register)
+        
+        
+        return None 
+    
+    
+    def temperature_enable(self, value: int) -> None:
+        
+        current_register = self.read_regsiter(PWR_CTRL)
+        if(value == 0x00):
+            current_register &= ~BIT_3
+        elif(value == 0x01):
+            current_register |= BIT_3 
+        else:
+            return None 
+        
+        
+        
+        self.temp_en = value 
+        self.serial_device.writeto_mem(I2C_PRIM_ADDR, PWR_CTRL, current_register)
+        
+        
+        return None 
+    
+    
+class Command_Register:
+    def __init__(self, serial_device: I2C) -> None:
+        
+        self.serial_device = serial_device 
+        
+        return None 
+    
+    
+    def g_trigger(self) -> None:
+        
+        self.serial_device.writeto_mem(I2C_PRIM_ADDR, CMD, 0x02)
+        
+        return None 
+    
+    
+    def usr_gain(self) -> None:
+        
+        self.serial_device.writeto_mem(I2C_PRIM_ADDR, CMD, 0x03)
+        
+        return None 
+    
+    
+    def nvm_prog(self) -> None:
+        
+        self.serial_device.writeto_mem(I2C_PRIM_ADDR, CMD, 0xa0)
+        
+        return None 
+    
+    def fifo_flush(self) -> None:
+        
+        self.serial_device.writeto_mem(I2C_PRIM_ADDR, CMD, 0xb0)
+        
+        return None 
+    
+    
+    def softreset(self) -> None:
+        
+        self.serial_device.writeto_mem(I2C_PRIM_ADDR, CMD, 0xb6)
+        
+        return None 
