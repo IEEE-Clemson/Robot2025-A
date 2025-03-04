dc1d1e794e84796f4e3a5243e7ce1dea0336c10b
Connor Belli <connorbelli2003@gmail.com>
2025-03-04 14:28:11 -0500

    Addeed documentation for encoder

--- micro/drivetrain_native/include/config.h
+++ micro/drivetrain_native/include/config.h
@@ -17,19 +17,27 @@ static const uint FREQ = 200;
 // I2C config
 #define BUFF_SIZE 128
 
+// Address of this pico over I2C
 static const uint I2C_SLAVE_ADDRESS = 0x41;
-static const uint I2C_BAUDRATE = 400000; // 400 kHz
+// Baud rate of I2C in hertz
+static const uint I2C_BAUDRATE = 400*1000;
 
+// GPIO number of SDA pin for slave I2C
 static const uint I2C_SLAVE_SDA_PIN = 20;
+// GPIO number of SCL pin for slave I2C
 static const uint I2C_SLAVE_SCL_PIN = 21;
 
 // IMU config
+// GPIO number of I2C SDA pin for use with IMU
 static const uint8_t IMU_SDA_PIN = 26;
+// GPIO number of I2C SCL pin for use with IMU
 static const uint8_t IMU_SCL_PIN = 27;
+// I2C instance to use with IMU, must be different from slave I2C
 static i2c_inst_t *IMU_I2C_INST = i2c1;
 
-// 
+// Maximum speed possible for usage in I2C communication
 static const float MAX_VXY = 2.0f;
+// Maximum angular velocity possible for usage in I2C communication
 static const float MAX_OMEGA = 8.0f;
 
 // MOTOR PIN CONFIG
--- micro/drivetrain_native/include/util.h
+++ micro/drivetrain_native/include/util.h
@@ -5,6 +5,11 @@
 extern "C" {
 #endif
 
+/// @brief Limits value x to between min_val and max_val I.e. [min_val, max_val]
+/// @param x Value to clamp
+/// @param min_val Minimum bound of output
+/// @param max_val Maximum bound of output
+/// @return Clamped value of x
 float clamp(float x, float min_val, float max_val);
 
 #ifdef __cplusplus
