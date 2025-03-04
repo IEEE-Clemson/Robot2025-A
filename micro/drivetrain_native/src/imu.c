59a60bc2dd857224cfc49c582c3071d0477a136c
Connor Belli <connorbelli2003@gmail.com>
2025-03-04 10:23:30 -0500

    Added registers for BNO055

--- micro/drivetrain_native/src/imu.c
+++ micro/drivetrain_native/src/imu.c
@@ -61,39 +61,109 @@ static int i2c_write_data(i2c_inst_t* i2c, uint8_t reg_addr, const uint8_t *reg_
 
 
 // BNO055 Registers
+// https://cdn-shop.adafruit.com/datasheets/BST_BNO055_DS000_12.pdf
 
-// I2C
-static const uint8_t I2C_BUS         = 1;
+static const uint8_t CHIP_ID              = 0x00;
+static const uint8_t ACC_ID               = 0x01;
+static const uint8_t MAG_ID               = 0x02;
+static const uint8_t GYR_ID               = 0x03;
+static const uint8_t SW_REV_ID_7_0        = 0x04;
+static const uint8_t SW_REV_ID_15_8       = 0x05;
+static const uint8_t BL_REV_ID            = 0x06;
 
-// General
-static const uint8_t CHIP_ID_ADDRESS = 0x00;
-static const uint8_t SENSORTIME_0    = 0x18;
-static const uint8_t PWR_CONF        = 0x7C;
-static const uint8_t PWR_CTRL        = 0x7D;
+static const uint8_t ACC_DATA_X_7_0       = 0x08;
+static const uint8_t ACC_DATA_X_15_8      = 0x09;
+static const uint8_t ACC_DATA_Y_7_0       = 0x0A;
+static const uint8_t ACC_DATA_Y_15_8      = 0x0B;
+static const uint8_t ACC_DATA_Z_7_0       = 0x0C;
+static const uint8_t ACC_DATA_Z_15_8      = 0x0D;
 
-// Accelerometer
-static const uint8_t ACC_CONF        = 0x40;
-static const uint8_t ACC_RANGE       = 0x41;
-static const uint8_t ACC_X_7_0       = 0x08;
-static const uint8_t ACC_X_15_8      = 0x09;
-static const uint8_t ACC_Y_7_0       = 0x0A;
-static const uint8_t ACC_Y_15_8      = 0x0B;
-static const uint8_t ACC_Z_7_0       = 0x0C;
-static const uint8_t ACC_Z_15_8      = 0x0D;
+static const uint8_t MAG_DATA_X_7_0       = 0x0E;
+static const uint8_t MAG_DATA_X_15_8      = 0x0F;
+static const uint8_t MAG_DATA_Y_7_0       = 0x10;
+static const uint8_t MAG_DATA_Y_15_8      = 0x11;
+static const uint8_t MAG_DATA_Z_7_0       = 0x12;
+static const uint8_t MAG_DATA_Z_15_8      = 0x13;
 
-// Gyroscope
-static const uint8_t GYR_CONF        = 0x42;
-static const uint8_t GYR_RANGE       = 0x43;
-static const uint8_t GYR_X_7_0       = 0x14;
-static const uint8_t GYR_X_15_8      = 0x15;
-static const uint8_t GYR_Y_7_0       = 0x16;
-static const uint8_t GYR_Y_15_8      = 0x17;
-static const uint8_t GYR_Z_7_0       = 0x18;
-static const uint8_t GYR_Z_15_8      = 0x19;
+static const uint8_t GYR_DATA_X_7_0       = 0x14;
+static const uint8_t GYR_DATA_X_15_8      = 0x15;
+static const uint8_t GYR_DATA_Y_7_0       = 0x16;
+static const uint8_t GYR_DATA_Y_15_8      = 0x17;
+static const uint8_t GYR_DATA_Z_7_0       = 0x18;
+static const uint8_t GYR_DATA_Z_15_8      = 0x19;
 
-// Temperature
-static const uint8_t TEMP_7_0        = 0x22;
-static const uint8_t TEMP_15_8       = 0x23;
+static const uint8_t EUL_DATA_X_7_0       = 0x1A;
+static const uint8_t EUL_DATA_X_15_8      = 0x1B;
+static const uint8_t EUL_DATA_Y_7_0       = 0x1C;
+static const uint8_t EUL_DATA_Y_15_8      = 0x1D;
+static const uint8_t EUL_DATA_Z_7_0       = 0x1E;
+static const uint8_t EUL_DATA_Z_15_8      = 0x1F;
+
+static const uint8_t QUAT_DATA_W_7_0      = 0x20;
+static const uint8_t QUAT_DATA_W_15_8     = 0x21;
+static const uint8_t QUAT_DATA_X_7_0      = 0x22;
+static const uint8_t QUAT_DATA_X_15_8     = 0x23;
+static const uint8_t QUAT_DATA_Y_7_0      = 0x24;
+static const uint8_t QUAT_DATA_Y_15_8     = 0x25;
+static const uint8_t QUAT_DATA_Z_7_0      = 0x26;
+static const uint8_t QUAT_DATA_Z_15_8     = 0x27;
+
+static const uint8_t LIA_DATA_X_7_0       = 0x28;
+static const uint8_t LIA_DATA_X_15_8      = 0x29;
+static const uint8_t LIA_DATA_Y_7_0       = 0x2A;
+static const uint8_t LIA_DATA_Y_15_8      = 0x2B;
+static const uint8_t LIA_DATA_Z_7_0       = 0x2C;
+static const uint8_t LIA_DATA_Z_15_8      = 0x2D;
+
+static const uint8_t GRV_DATA_X_7_0       = 0x2E;
+static const uint8_t GRV_DATA_X_15_8      = 0x2F;
+static const uint8_t GRV_DATA_Y_7_0       = 0x30;
+static const uint8_t GRV_DATA_Y_15_8      = 0x31;
+static const uint8_t GRV_DATA_Z_7_0       = 0x32;
+static const uint8_t GRV_DATA_Z_15_8      = 0x33;
+
+static const uint8_t TEMP                 = 0x34;
+
+static const uint8_t CALIB_STAT           = 0x35;
+static const uint8_t ST_RESULT            = 0x36;
+static const uint8_t INT_STA              = 0x37;
+static const uint8_t SYS_CLK_STATUS       = 0x38;
+static const uint8_t SYS_STATUS           = 0x39;
+static const uint8_t SYS_ERR              = 0x3A;
+
+static const uint8_t UNIT_SEL             = 0x3B;
+static const uint8_t OPR_MODE             = 0x3D;
+static const uint8_t PWR_MODE             = 0x3E;
+static const uint8_t SYS_TRIGGER          = 0x3F;
+static const uint8_t TEMP_SOURCE          = 0x40;
+static const uint8_t AXIS_MAP_CONFIG      = 0x41;
+static const uint8_t AXIS_MAP_SIGN        = 0x42;
+
+static const uint8_t ACC_OFFSET_X_7_0     = 0x55;
+static const uint8_t ACC_OFFSET_X_15_8    = 0x56;
+static const uint8_t ACC_OFFSET_Y_7_0     = 0x57;
+static const uint8_t ACC_OFFSET_Y_15_8    = 0x58;
+static const uint8_t ACC_OFFSET_Z_7_0     = 0x59;
+static const uint8_t ACC_OFFSET_Z_15_8    = 0x5A;
+
+static const uint8_t MAG_OFFSET_X_7_0     = 0x5B;
+static const uint8_t MAG_OFFSET_X_15_8    = 0x5C;
+static const uint8_t MAG_OFFSET_Y_7_0     = 0x5D;
+static const uint8_t MAG_OFFSET_Y_15_8    = 0x5E;
+static const uint8_t MAG_OFFSET_Z_7_0     = 0x5F;
+static const uint8_t MAG_OFFSET_Z_15_8    = 0x60;
+
+static const uint8_t GYR_OFFSET_X_7_0     = 0x61;
+static const uint8_t GYR_OFFSET_X_15_8    = 0x62;
+static const uint8_t GYR_OFFSET_Y_7_0     = 0x63;
+static const uint8_t GYR_OFFSET_Y_15_8    = 0x64;
+static const uint8_t GYR_OFFSET_Z_7_0     = 0x65;
+static const uint8_t GYR_OFFSET_Z_15_8    = 0x66;
+
+static const uint8_t ACC_RADIUS_7_0       = 0x67;
+static const uint8_t ACC_RADIUS_15_8      = 0x68;
+static const uint8_t MAG_RADIUS_7_0       = 0x69;
+static const uint8_t MAG_RADIUS_15_8      = 0x6A;
 
 // Common Definitions
 // General
@@ -192,17 +262,17 @@ void bno_init(struct BNO055 *bno, i2c_inst_t *i2c, uint8_t sda, uint8_t scl)
 void bno_get_raw_gyr_data(struct BNO055* bmi, int16_t *gx, int16_t* gy, int16_t*gz)
 {
     uint16_t gx1, gx0, gy1, gy0, gz1, gz0;
-    gx0 = read_register(bmi, GYR_X_7_0);
-    gx1 = read_register(bmi, GYR_X_15_8);
+    gx0 = read_register(bmi, GYR_DATA_X_7_0);
+    gx1 = read_register(bmi, GYR_DATA_X_15_8);
     *gx = (gx1 << 8) | gx0;
 
 
-    gy0 = read_register(bmi, GYR_Y_7_0);
-    gy1 = read_register(bmi, GYR_Y_15_8);
+    gy0 = read_register(bmi, GYR_DATA_Y_7_0);
+    gy1 = read_register(bmi, GYR_DATA_Y_15_8);
     *gy = (gy1 << 8) | gy0;
 
-    gz0 = read_register(bmi, GYR_Z_7_0);
-    gz1 = read_register(bmi, GYR_Z_15_8);
+    gz0 = read_register(bmi, GYR_DATA_Z_7_0);
+    gz1 = read_register(bmi, GYR_DATA_Z_15_8);
     *gz = (gz1 << 8) | gz0;
 }
 
