99b0e161d27f744dd2ee7fa43be79fb82b6ba1d2
Connor Belli <connorbelli2003@gmail.com>
2025-03-27 13:06:07 -0400

    Added commands to wait until led is on

---
+++ subsystems/safety.py
@@ -0,0 +1,14 @@
+from commands2 import Subsystem
+
+from cu_hal.interfaces import AuxilliaryHAL
+
+
+class Safety(Subsystem):
+    def __init__(self, hal: AuxilliaryHAL):
+        self.hal = hal
+
+    def is_armed(self) -> bool:
+        return self.hal.is_armed()
+
+    def is_light_on(self) -> bool:
+        return self.hal.is_led_on()
